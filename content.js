24292e9b504053b073e49834818423128df2b076
Sumedh314 <sumedh3141@gmail.com>
2026-04-05 01:46:10 -0500

    Program displays new grade when user edits points

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
-const allClassGrades = {};
-const classCategoryWeights = {};
-const realClassAssignmentGrades = {};
-let userClassAssignmentGrades = {};
+const finalGradesByClass = {};
+const categoryWeightsByClass = {};
+const realGradesByClass = {};
+let newGradesByClass = {};
 
 const iframe = document.getElementById('sg-legacy-iframe');
 /** @type {HTMLElement} */ let content = null;
@@ -13,25 +13,27 @@ function getData() {
         const className = gradeHeader.querySelector('.sg-header-heading').textContent.trim();
         const classGrade = gradeHeader.querySelector('.sg-header-heading.sg-right').textContent.trim();
 
-        allClassGrades[className] = classGrade;
+        finalGradesByClass[className] = classGrade;
 
         const categoriesTable = userClass.querySelector('.sg-content-grid').querySelector('.sg-asp-table-group').querySelector('table');
         const categoryRows = categoriesTable.querySelectorAll('.sg-asp-table-data-row');
 
-        const weights = {};
+        const weightsByCategory = {};
         categoryRows.forEach(row => {
             const rowElements = row.getElementsByTagName('td');
             const categoryName = rowElements[0].textContent.trim();
-            const categoryWeight = rowElements.length > 4 ? rowElements[4].textContent.trim() : '100';
             
-            weights[categoryName] = categoryWeight;
+            const originalWeightValue = Number(rowElements[4]?.textContent.trim());
+            const categoryWeight = rowElements.length > 4 ? originalWeightValue > 1 ? originalWeightValue / 100 : originalWeightValue : 1;
+            
+            weightsByCategory[categoryName] = categoryWeight;
 
-            classCategoryWeights[className] = weights;
+            categoryWeightsByClass[className] = weightsByCategory;
         });
         
         const assignmentRows = userClass.querySelector('.sg-asp-table').querySelectorAll('.sg-asp-table-data-row');
         
-        const grades = {};
+        const gradeDataByAssignment = {};
         assignmentRows.forEach(row => {
             const rowElements = row.getElementsByTagName('td');
 
@@ -40,27 +42,31 @@ function getData() {
             const gradeData = {
                 category: '',
                 score: '',
-                maxScore: ''
+                maxScore: '',
+                scoreElement: null
             };
-            const assignmentCategory = rowElements[3];
-            const assignmentScore = rowElements[4];
-            const maximumScore = rowElements[5];
+            const assignmentCategory = rowElements[3].textContent.trim();
+            const assignmentScore = rowElements[4].textContent.trim();
+            const maximumScore = rowElements[5].textContent.trim();
+            const assignmentScoreElement = rowElements[4];
 
             gradeData.category = assignmentCategory;
             gradeData.score = assignmentScore;
             gradeData.maxScore = maximumScore;
+            gradeData.scoreElement = assignmentScoreElement;
 
-            grades[assignmentName] = gradeData;
+            gradeDataByAssignment[assignmentName] = gradeData;
 
-            realClassAssignmentGrades[className] = grades;
-            userClassAssignmentGrades = realClassAssignmentGrades;
+            realGradesByClass[className] = gradeDataByAssignment;
+            newGradesByClass = realGradesByClass;
         });
     }
 }
 
 function addTextFields() {
-    const userClasses = allGradeTables.querySelectorAll('.AssignmentClass');
-    userClasses.forEach(userClass => {
+    const userClassElements = allGradeTables.querySelectorAll('.AssignmentClass');
+
+    userClassElements.forEach(userClass => {
         const newGradeArea = document.createElement('input');
         newGradeArea.style.width = '110px';
         newGradeArea.style.height = '25px';
@@ -74,37 +80,66 @@ function addTextFields() {
         gradeElement.appendChild(newGradeArea);
     });
 
-    Object.values(realClassAssignmentGrades).forEach(grades => {
+    Object.values(realGradesByClass).forEach(grades => {
         Object.values(grades).forEach(data => {
             const inputArea = document.createElement('input');
             inputArea.style.width = '40px';
-            inputArea.defaultValue = data.score.textContent.trim();
+            inputArea.defaultValue = data.score;
 
-            data.score.appendChild(inputArea);
+            data.scoreElement.appendChild(inputArea);
         });
     });
 }
 
-function updateGrade(event) {
-    const score = event.target.parentNode;
+function updateClassGrade(event) {
+    const gradeElement = event.target.parentNode;
 
-    const assignmentElement = score.parentNode.getElementsByTagName('td')[2];
+    const assignmentElement = gradeElement.parentNode.getElementsByTagName('td')[2];
     const assignmentName = assignmentElement.querySelector('a').textContent.trim();
-    const assignmentWeight = score.parentNode.getElementsByTagName('td')[3].textContent.trim();
-    
+
     const classHeader = assignmentElement.closest('.AssignmentClass');
     const className = classHeader.querySelector('.sg-header-heading').textContent.trim();
 
-    userClassAssignmentGrades[className][assignmentName].score = event.target.value;
+    newGradesByClass[className][assignmentName].score = event.target.value;
 
-    console.log(classCategoryWeights[className][assignmentWeight]);
+    const totalUserPointsByCategory = {};
+    const totalMaxPointsByCategory = {};
+    const weightedScoresByCategory = {};
+    let finalGrade = 0;
+
+    Object.values(newGradesByClass[className]).forEach(assignment => {
+        const category = assignment.category;
+        let score = assignment.score;
+        let maxScore = Number(assignment.maxScore);
+        if (score[0] == 'X' || score[0] == 'x' || score == '') {
+            score = 0;
+            maxScore = 0;
+        }
+        else if (score[0] == 'Z' || score[0] == 'z') {
+            score = 0;
+        }
+        score = Number(score);
+
+        totalUserPointsByCategory[category] = (totalUserPointsByCategory[category] || 0) + score;
+        totalMaxPointsByCategory[category] = (totalMaxPointsByCategory[category] || 0) + maxScore;
+    });
+
+    Object.keys(totalUserPointsByCategory).forEach(category => {
+        const weight = Number(categoryWeightsByClass[className][category]);
+        weightedScoresByCategory[category] = totalUserPointsByCategory[category] / totalMaxPointsByCategory[category] * weight;
+    });
+
+    Object.values(weightedScoresByCategory).forEach(score => finalGrade += score);
+    
+    const newGradeArea = classHeader.querySelector('.sg-header.sg-header-square').querySelector('input');
+    newGradeArea.value = (finalGrade * 100).toPrecision(4) + '%';
 }
 
 iframe.addEventListener('load', () => {
     content = iframe.contentDocument || iframe.contentWindow.document;
     allGradeTables = content.getElementById('plnMain_pnlFullPage');
 
-    content.addEventListener('input', (event) => updateGrade(event));
+    content.addEventListener('input', (event) => updateClassGrade(event));
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
