932be91d6359791b5970f45330182a0191cc02bb
Sumedh314 <sumedh3141@gmail.com>
2026-04-27 11:19:42 -0500

    User added assignments used in grade calculation

--- background.js
+++ background.js
@@ -8,19 +8,24 @@ chrome.action.onClicked.addListener(async (tab) => {
     chrome.tabs.sendMessage(tab.id, { action: message });
     chrome.storage.local.set({ activated: newStatus });
 });
-chrome.tabs.onUpdated.addListener(() => chrome.storage.local.set({ activated: false }));
-chrome.runtime.onInstalled.addListener(() => {
-    console.log('hi');
-    chrome.tabs.reload();
-    chrome.storage.local.set({ activated: false });
-});
-chrome.commands.onCommand.addListener(command => command == 'reload_extension' && chrome.runtime.reload());
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action == 'injectCSS') {
         chrome.scripting.insertCSS({
             target: { tabId: sender.tab.id, allFrames: true },
             files: ['content_script/content.css']
         });
+        sendResponse('successful');
     }
     sendResponse();
-})
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.clear();
+    chrome.storage.local.set({ allNewGrades: {} });
+    chrome.tabs.reload();
+    chrome.storage.local.set({ activated: false });
+});
+
+chrome.tabs.onUpdated.addListener(() => chrome.storage.local.set({ activated: false }));
+chrome.commands.onCommand.addListener(command => command == 'reload_extension' && chrome.runtime.reload());
--- content_script/content.js
+++ content_script/content.js
@@ -1,92 +1,12 @@
 const overallClassGrades = {}; // Overall grades for each class             {className: classOverallGrade}
 const classCategoryWeights = {}; // Category weights in each class          {className: {category: weight}}
-const allRealGrades = {}; // Grades for each assignment in each class       {className: {assignmentName: {grade data...}}}
-let allNewGrades = {}; // Same as allRealGrades, but based on user entries  {className: {assignmentName: {grade data...}}}
+const allRealGrades = {}; // Grades for each assignment in each class       {className: {assignmentIndex: {grade data...}}}
+let allNewGrades = {}; // Same as allRealGrades, but based on user entries  {className: {assignmentIndex: {grade data...}}}
 
 // Content inside Iframe
 const iframe = document.getElementById('sg-legacy-iframe');
 /** @type {HTMLElement} */ let content = null;
 /** @type {HTMLElement} */ let allGradeTables = null;
-
-/**
- * Gets the user's grades and assignment weights and stores them in the corresponding objects.
- */
-async function getData() {
-
-    // Loop through each class to get all the data
-    for (const userClassElement of allGradeTables.querySelectorAll('.AssignmentClass')) {
-
-        // Fill object that stores user's overall class grades
-        const gradeHeader = userClassElement.querySelector('.sg-header.sg-header-square');
-        const className = gradeHeader.querySelector('.sg-header-heading').textContent.trim();
-        const classGrade = gradeHeader.querySelector('.sg-header-heading.sg-right').textContent.trim();
-        overallClassGrades[className] = classGrade;
-
-        // Get ready to fill object that stores the weights of each category
-        const categoryWeights = {}; // Inner object to store category weights for a specific class {category: weigiht}
-        const categoriesTable = userClassElement.querySelector('.sg-content-grid').querySelector('.sg-asp-table-group').querySelector('table');
-        const categoryRows = categoriesTable.querySelectorAll('.sg-asp-table-data-row');
-
-        // Loop through each row in the table that stores weights for each category
-        categoryRows.forEach(row => {
-
-            // Get category names and original weights
-            const rowElements = row.getElementsByTagName('td');
-            const categoryName = rowElements[0].textContent.trim();
-            const originalWeightValue = Number(rowElements[4]?.textContent.trim());
-            let categoryWeight = '';
-
-            // Make sure weight is between 0 and 1. If there is only one category, its weight is 1.
-            if (rowElements.length > 4) {
-                categoryWeight = originalWeightValue > 1 ? originalWeightValue / 100 : originalWeightValue;
-            }
-            else {
-                categoryWeight = 1;
-            }
-            
-            // Add values to objects
-            categoryWeights[categoryName] = categoryWeight;
-            classCategoryWeights[className] = categoryWeights;
-        });
-        
-        // Get ready to fill object that stores all the user's real grades
-        const assignmentData = {}; // Inner object to store grades for each assignment in a specific class {assignment: grade}
-        const assignmentRows = userClassElement.querySelector('.sg-asp-table').querySelectorAll('.sg-asp-table-data-row');
-        
-        // Loop through each row in the table that stores all the user's assignments for a class
-        assignmentRows.forEach(row => {
-
-            // Get the name of the assignment in the current row
-            const rowElements = row.getElementsByTagName('td');
-            const assignmentName = rowElements[2].querySelector('a').textContent.trim();
-
-            // Store all the relevent data for each assignment grade
-            const gradeData = {
-                category: rowElements[3].textContent.trim(),
-                score: rowElements[4].textContent.trim(),
-                maxScore: rowElements[5].textContent.trim(),
-                weight: rowElements[6].textContent.trim(),
-                scoreElement: rowElements[4]
-            };
-
-            // Add values to objects
-            assignmentData[assignmentName] = gradeData;
-            allRealGrades[className] = assignmentData;
-        });
-    }
-
-    const allNewGradesData = await chrome.storage.local.get('allNewGrades');
-    allNewGrades = allNewGradesData.allNewGrades;
-
-    if (Object.keys(allNewGrades).length == 0) {
-        allNewGrades = allRealGrades;
-        chrome.storage.local.set({ allNewGrades: allNewGrades });
-        // console.log(allNewGrades);
-    }
-    else {
-        // console.log(allNewGrades);
-    }
-}
 
 /**
  * Calculates the new grade of a class after the user enters a new hypothetical grade
@@ -96,16 +16,20 @@ async function getData() {
 function updateClassGrade(event) {
     
     // Get assignment name based on the row of the text field that was updated
-    const assignmentRowElement = event.target.parentNode.parentNode.getElementsByTagName('td')[2];
-    const assignmentName = assignmentRowElement.querySelector('a').textContent.trim();
-
+    const assignmentRowElement = event.target.parentNode.parentNode;
+    const gradeArray = Array.from(assignmentRowElement.parentNode.children);
+    console.log(gradeArray);
+    const assignmentIndex = gradeArray.length - gradeArray.indexOf(assignmentRowElement) - 1;
+    
     // Get class name based on the table that the text field selected was inside
     const classHeader = assignmentRowElement.closest('.AssignmentClass');
     const className = classHeader.querySelector('.sg-header-heading').textContent.trim();
 
     // Update object and local storage to store new value user entered
-    allNewGrades[className][assignmentName].score = event.target.value;
+    // console.log(assignmentData);
+    allNewGrades[className][assignmentIndex][event.target.dataset.type] = event.target.value;
     chrome.storage.local.set({ allNewGrades: allNewGrades });
+    console.log(allNewGrades);
 
     // Objects to store data for each category to help calculate final grade
     const totalCategoryUserPoints = {}; // Total points user has in each category           {category: points}
@@ -116,11 +40,11 @@ function updateClassGrade(event) {
         const category = assignment.category;
         let score = assignment.score;
         let maxScore = Number(assignment.maxScore);
-        if (score == '' || score[0].toUpperCase() == 'X') {
+        if (score == '' || score == 'X') {
             score = 0;
             maxScore = 0;
         }
-        else if (score[0].toUpperCase() == 'Z') {
+        else if (score == 'Z') {
             score = 0;
         }
         score = Number(score);
@@ -141,6 +65,50 @@ function updateClassGrade(event) {
     // Update final grade shown to user
     const newGradeArea = classHeader.querySelector('.sg-header.sg-header-square').querySelector('input');
     newGradeArea.value = (finalGrade * 100).toPrecision(4) + '%';
+}
+
+/**
+ * Adds an extra row to the class where the user clicked the button. This allows the user to enter
+ * a new hypothetical assignment rather than only editing an already existing grade.
+ * 
+ * @param {object} event Button which user clicked to add assignment
+ */
+function addAssignment(event) {
+    const className = event.target.closest('.AssignmentClass').querySelector('.sg-header-heading').textContent.trim();
+    const newIndex = Math.max(...Object.keys(allNewGrades[className])) + 1;
+
+    event.target.closest('table').style.tableLayout = 'fixed';
+    const newAssignmentRow = document.createElement('tr');
+    newAssignmentRow.classList.add('extension', 'assignment-row');
+
+    let scoreElement = null;
+    const inputData = ['dateDue', 'dateAssigned', 'name', 'category', 'score', 'maxScore']
+    for (let index = 0; index < 6; index++) {
+        const cell = document.createElement('td');
+        const textInput = document.createElement('input');
+        textInput.classList.add('extension', 'grade-entry');
+        cell.insertAdjacentElement('beforeend', textInput);
+        newAssignmentRow.insertAdjacentElement('beforeend', cell);
+
+        textInput.dataset.type = inputData[index];
+        if (index == 5) {
+            scoreElement = textInput;
+        }
+    }
+
+    const gradeData = {
+        name: '',
+        category: '',
+        score: '',
+        maxScore: '',
+        weight: '',
+        scoreElement: scoreElement,
+        userCreated: true
+    }
+
+    event.target.parentNode.parentNode.insertAdjacentElement('afterend', newAssignmentRow);
+
+    allNewGrades[className][newIndex] = gradeData;
 }
 
 /**
@@ -187,40 +155,32 @@ async function addFunctionality() {
 
     // Add text fields next to each grade in every class
     for (const userClass of Object.keys(allRealGrades)) {
-
+        console.log(allNewGrades);
+        
         // Loop through all classes
-        for (const assignmentName of Object.keys(allRealGrades[userClass])) {
-            const assignment = allRealGrades[userClass][assignmentName];
-
+        let index = 1;
+        for (const assignmentIndex of Object.keys(allRealGrades[userClass])) {
+            // console.log(index);
+            index++;
+            
             // Create text field
             const inputArea = document.createElement('input');
-            inputArea.classList.add('extension', 'text-field');
-
-            // Update default value of text fields to represent the same value as original grades
-            // If the grade starts with X (exempt) or Z (late), make those the default value
-            console.log(userClass);
-            if (allNewGrades[userClass]?.[assignmentName]?.score != undefined) {
-                inputArea.defaultValue = allNewGrades[userClass][assignmentName].score;
+            inputArea.classList.add('extension', 'grade-entry');
+            inputArea.id = 'score';
+            
+            // Set the default value of grades to be from what the user entered from previous uses of the extension
+            let gradeShown = '';
+            if (allNewGrades[userClass]?.[assignmentIndex]?.score != undefined) {
+                gradeShown = allNewGrades[userClass][assignmentIndex].score;
             }
-
-            else if (assignment.score[0]?.toUpperCase() == 'X' || assignment.score[0]?.toUpperCase() == 'Z') {
-                inputArea.defaultValue = assignment.score[0].toUpperCase();
-            }
-
-            // If the grade is an integer, remove its decimals
-            else if (Math.round(assignment.score) == assignment.score) {
-                inputArea.defaultValue = Number(assignment.score).toFixed(0);
-            }
-
-            // Otherwise, simply use the same value
-            else {
-                inputArea.defaultValue = assignment.score;
-            }
-
-            // Add text field right next to actual grade
-            assignment.scoreElement.insertAdjacentElement('beforeend', inputArea);
+            inputArea.defaultValue = gradeShown;
+            
+            // console.log(allNewGrades[userClass]);
+            allNewGrades[userClass][assignmentIndex].scoreElement.insertAdjacentElement('beforeend', inputArea);
+            allNewGrades[userClass][assignmentIndex].score = gradeShown;
         }
     }
+    console.log('asdf;klajs;dlfkas');
 
     // When the content is clicked, see if the "Add assignment" button was clicked
     content.addEventListener('click', detectAssignmentButtonClick);
@@ -230,22 +190,94 @@ async function addFunctionality() {
 }
 
 /**
- * Adds an extra row to the class where the user clicked the button. This allows the user to enter
- * a new hypothetical assignment rather than only editing an already existing grade.
- * 
- * @param {object} event Button which user clicked to add assignment
+ * Gets the user's grades and assignment weights and stores them in the corresponding objects.
  */
-function addAssignment(event) {
-    event.target.closest('table').style.tableLayout = 'fixed';
-    const newAssignmentRow = document.createElement('tr');
-    newAssignmentRow.classList.add('extension', 'assignment-row');
-    for (let i = 0; i < 6; i++) {
-        const cell = document.createElement('td');
-        // cell.style.width = 'auto';
-        cell.insertAdjacentElement('beforeend', document.createElement('input'));
-        newAssignmentRow.insertAdjacentElement('beforeend', cell);
+async function getData() {
+
+    // Loop through each class to get all the data
+    for (const userClassElement of allGradeTables.querySelectorAll('.AssignmentClass')) {
+
+        // Fill object that stores user's overall class grades
+        const gradeHeader = userClassElement.querySelector('.sg-header.sg-header-square');
+        const className = gradeHeader.querySelector('.sg-header-heading').textContent.trim();
+        const classGrade = gradeHeader.querySelector('.sg-header-heading.sg-right').textContent.trim();
+        overallClassGrades[className] = classGrade;
+
+        // Get ready to fill object that stores the weights of each category
+        const categoryWeights = {}; // Inner object to store category weights for a specific class {category: weigiht}
+        const categoriesTable = userClassElement.querySelector('.sg-content-grid').querySelector('.sg-asp-table-group').querySelector('table');
+        const categoryRows = categoriesTable.querySelectorAll('.sg-asp-table-data-row');
+
+        // Loop through each row in the table that stores weights for each category
+        categoryRows.forEach(row => {
+
+            // Get category names and original weights
+            const rowElements = row.getElementsByTagName('td');
+            const categoryName = rowElements[0].textContent.trim();
+            const originalWeightValue = Number(rowElements[4]?.textContent.trim());
+            let categoryWeight = '';
+
+            // Make sure weight is between 0 and 1. If there is only one category, its weight is 1.
+            if (rowElements.length > 4) {
+                categoryWeight = originalWeightValue > 1 ? originalWeightValue / 100 : originalWeightValue;
+            }
+            else {
+                categoryWeight = 1;
+            }
+            
+            // Add values to objects
+            categoryWeights[categoryName] = categoryWeight;
+            classCategoryWeights[className] = categoryWeights;
+        });
+        
+        // Get ready to fill object that stores all the user's real grades
+        const assignmentData = {}; // Inner object to store grades for each assignment in a specific class {assignment: grade}
+        const assignmentRows = Array.from(userClassElement.querySelector('.sg-asp-table').querySelectorAll('.sg-asp-table-data-row')).reverse();
+        
+        // Loop through each row in the table that stores all the user's assignments for a class
+        let index = 0;
+        assignmentRows.forEach(row => {
+
+            // Get the name of the assignment in the current row
+            const rowElements = row.getElementsByTagName('td');
+            
+            // Store all the relevent data for each assignment grade
+            const gradeData = {
+                name: rowElements[2].querySelector('a').textContent.trim(),
+                category: rowElements[3].textContent.trim(),
+                score: rowElements[4].textContent.trim(),
+                maxScore: rowElements[5].textContent.trim(),
+                weight: rowElements[6].textContent.trim(),
+                scoreElement: rowElements[4],
+                userCreated: false
+            };
+
+            // If the grade starts with X (exempt) or Z (late), make those the default value
+            if (gradeData.score[0]?.toUpperCase() == 'X' || gradeData.score[0]?.toUpperCase() == 'Z') {
+                gradeData.score = gradeData.score[0].toUpperCase();
+            }
+            
+            // If the grade is not blank and the grade is an integer, remove its decimals
+            else if (gradeData.score != '' && Math.round(gradeData.score) == gradeData.score) {
+                gradeData.score = Number(gradeData.score).toFixed(0);
+            }
+            
+            // Add values to objects
+            assignmentData[index] = gradeData;
+            allRealGrades[className] = assignmentData;
+
+            index++;
+        });
     }
-    event.target.parentNode.parentNode.insertAdjacentElement('afterend', newAssignmentRow);
+
+    // const allNewGradesData = await chrome.storage.local.get('allNewGrades');
+    // allNewGrades = allNewGradesData.allNewGrades;
+
+    // if (Object.keys(allNewGrades).length == 0) {
+    allNewGrades = allRealGrades;
+    //     chrome.storage.local.set({ allNewGrades: allNewGrades });
+    // }
+    console.log(allNewGrades);
 }
 
 /**
@@ -259,17 +291,20 @@ function detectUserGradeUpdate()  {
 
     // Update grade when user focuses out of a text box or presses enter
     content.addEventListener('focusout', (event) => {
-
-        if (event.target.classList.contains('text-field')) {
+        if (event.target.classList.contains('grade-entry')) {
             updateClassGrade(event);
         }
     });
     content.addEventListener('keypress', (event) => {
-        if (event.key === 'Enter' && content.activeElement.classList.contains('text-field')) {
+        if (event.key === 'Enter' && content.activeElement.classList.contains('grade-entry')) {
             event.preventDefault();
             updateClassGrade(event);
         }
     });
+
+    // Adjust Iframe size when content is changed
+    const observer = new MutationObserver(resizeContent);
+    observer.observe(content.querySelector('body'), {attributes: true, childList: true, subtree: true});
 }
 
 /**
@@ -296,6 +331,14 @@ function detectAssignmentButtonClick(event) {
     }
 }
 
+/**
+ * Adjusts height of Iframe that contains grades when content changes
+ */
+function resizeContent() {
+    iframe.style.height = `${content.querySelector('html').querySelector('body').clientHeight}px`;
+    // console.log(iframe.style.height);
+}
+
 // Runs when the user's grades are loaded
 iframe.addEventListener('load', detectUserGradeUpdate);
 
@@ -314,6 +357,3 @@ chrome.runtime.onMessage.addListener(async (message, _, sendResponse) => {
     sendResponse();
     return true;
 });
-
-// const observer = new MutationObserver(getData);
-// observer.observe(document.body, {attributes: true, childList: true, subtree: true});
